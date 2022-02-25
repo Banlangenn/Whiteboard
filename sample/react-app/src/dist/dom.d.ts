2a863229f9c4f5c3a33c820818a77a0ff3b463eb
langen <langen@dian.so>
2022-02-25 11:33:52 +0800

    feat: 优化demo

--- sample/react-app/src/dist/dom.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function parseDom(html: string): any;
-export declare function addEvent(el: HTMLElement, type: string, fn: EventListenerOrEventListenerObject, capture?: AddEventListenerOptions): void;
-export declare function removeEvent(el: HTMLElement, type: string, fn: EventListenerOrEventListenerObject, capture?: EventListenerOptions): void;
-//# sourceMappingURL=dom.d.ts.map
--- sample/react-app/src/dist/model/domManager.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function createImage(container: HTMLElement, src: string, isShow: boolean, zIndex: string, width?: number, height?: number): HTMLImageElement;
-export declare function domShow(dom: HTMLElement): void;
-export declare function domHide(dom: HTMLElement): void;
-export declare function domMove(dom: HTMLElement, x: number, y: number): void;
-//# sourceMappingURL=domManager.d.ts.map
